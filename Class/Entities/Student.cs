f6d99e8bf1024c766aebf47ad0af60142b517f10
UEH <UEH@N501PC39>
2026-08-20 16:03:10 +0700

    Add project files.

---
+++ Class/Entities/Student.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class.Entities
+{
+    public class Student
+    {
+        private string name;
+        private double score;
+        public static int totalStudent;
+
+        public Student(string name, double score)
+        {
+            this.name = name;
+            this.score = score;
+            totalStudent++;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public double GetScore()
+        {
+            return score;
+        }
+
+        public bool IsPassed()
+        {
+            if (score >= 5.0)
+                return true;
+            else
+                return false;
+        }
+
+        public string GetClassfication()
+        {
+            string result = "";
+            if (score >= 8)
+                result = "Excellent";
+            else if (score >= 6.5)
+                result = "Good";
+            else if (score >= 5)
+                result = "Average";
+            else
+                result = "Weak";
+
+            return result;
+        }
+
+        public static int GetTotalStudent()
+        {
+            return totalStudent;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Name: {name} | Score: {score}");
+            Console.WriteLine("Passed: " + IsPassed());
+            Console.WriteLine("Classfication: " + GetClassfication());
+            Console.WriteLine("The total of student: " + GetTotalStudent());
+        }
+
+        public static Student FindTopStudent(Student[] students)
+        {
+            Student Top = students[0];
+            for (int i = 1; i < students.Length; i++)
+            {
+                if (students[i].score > Top.score)
+                {
+                    Top = students[i];
+                }
+            }
+            return Top;
+        }
+
+        // Đã chuyển thành static để gọi trực tiếp từ Class Student
+        public static double CalculateAverageScore(Student[] students)
+        {
+            double sum = 0;
+            for (int i = 0; i < students.Length; i++)
+            {
+                sum += students[i].score;
+            }
+            double avgscore = sum / students.Length;
+            return avgscore;
+        }
+    }
+}
---
+++ Class/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Class.Entities;
+
+namespace Class
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student("David", 9);
+            Student s2 = new Student("Anna", 8);
+            Student s3 = new Student("Lily", 7);
+
+            s1.DisplayInfo();
+            s2.DisplayInfo();
+            s3.DisplayInfo();
+
+            // Tạo mảng danh sách học sinh
+            Student[] students = { s1, s2, s3 };
+
+            // Tìm học sinh giỏi nhất
+            Student Top = Student.FindTopStudent(students);
+            Console.WriteLine("\nTop student: " + Top.GetName());
+            Console.WriteLine("Top score: " + Top.GetScore());
+            Console.WriteLine();
+
+            // Gọi hàm tính điểm trung bình qua static
+            double avg = Student.CalculateAverageScore(students);
+            Console.WriteLine("Average score: " + avg);
+
+            Console.ReadLine(); // Giữ màn hình Console
+        }
+    }
+}
